e59b852837f42cdbaec4909e6b6bce95b87720e6
Pedro Miguel <pedro.miguel.e.idk@gmail.com>
2023-04-08 11:28:52 -0300

    Card item made
    Card item framework made

--- app/src/main/java/com/example/courses/MainActivity.kt
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.courses
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.courses.data.Courses
 import com.example.courses.ui.theme.CoursesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    CoursesApp()
                 }
             }
         }
@@ -30,14 +39,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CoursesApp(){
+    Column() {
+        CoursesGridItem()
+    }
+}
+@Composable
+fun CoursesGridItem(){
+    Card(
+        modifier = Modifier.padding(8.dp),
+        elevation = 8.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(painter = painterResource(id = R.drawable.culinary),
+                contentDescription = stringResource(id = R.string.culinary))
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp)
+
+            ) {
+                Text(text = stringResource(id = R.string.culinary),
+                    fontWeight = FontWeight(weight =300),
+                    fontSize = 14.sp)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                        modifier = Modifier.width(12.dp).height(12.dp).padding(end = 4.dp)
+                        )
+                    Text(text = "321", fontSize = 10.sp)
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CoursesTheme {
-        Greeting("Android")
+        CoursesApp()
     }
 }
